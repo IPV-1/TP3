fbf263a0afc6069b6033dbe5362f54dd022cf330
lalo <leandro.gz73@gmail.com>
2014-06-10 23:08:44 -0300

    Configure jbundle

--- pacman-reloaded/src/main/java/map/color_parser.rb
+++ pacman-reloaded/src/main/java/map/color_parser.rb
@@ -2,23 +2,48 @@ require 'java'
 
 java_import Java::resource::Resource
 java_import Java::javax.imageio.ImageIO
+java_import Java::java.awt.image.BufferedImage
+java_import Java::com.uqbar.vainilla.appearances.Sprite
 
 class ColorParser
-  attr_accessor :walking_matrix
+  attr_accessor :walking_matrix, :background
 
   def initialize
     self.walking_matrix = []
   end
 
   def parse(file)
+    read_image(file) do |image,w, h|
+      (0...h).each do |i|
+        create_row(i, w, image)
+      end
+    end
+    read_image(file) do |image,w, h|
+      new_image = BufferedImage.new w, h, BufferedImage::TYPE_INT_ARGB
+      graphics = new_image.createGraphics
+      (0...h).each do |i|
+        create_image_row(i, w, image, graphics)
+      end
+      graphics.dispose
+      Sprite.new new_image
+    end
+
+  end
+
+  def read_image(file)
     stream = Resource.java_class.resource file
     image = ImageIO.read stream
     w = image.width
     h = image.height
-    (0...h).each do |i|
-      create_row(i, w, image)
-    end
+    yield image, w, h
+  end
 
+  def create_image_row(i, width, image, graphics)
+    (0...width).each do |j|
+      pixel = image.getRGB(j, i)
+      g_image = image_for(pixel, image)
+      graphics.draw_image(g_image, nil, i, j)
+    end
   end
 
   def create_row(i, width, image)
@@ -41,8 +66,10 @@ class ColorParser
     color == -324853
   end
 
-  def background
-
+  def image_for(color, image)
+    new_image = BufferedImage.new 29, 15, BufferedImage::TYPE_INT_ARGB
+    graphics = new_image.createGraphics
+    graphics.fillRect((int) component.getX(), (int) component.getY(), this.width, this.height);
   end
 end
 
