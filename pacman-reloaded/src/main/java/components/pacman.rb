7f4d33034622c6c259d558f9c52e1d0dac5d99a8
lalo <leandro.gz73@gmail.com>
2014-06-13 00:44:44 -0300

    add shape to pacman

--- pacman-reloaded/src/main/java/components/pacman.rb
+++ pacman-reloaded/src/main/java/components/pacman.rb
@@ -1,9 +1,24 @@
+require 'java'
+
+java_import Java::extensions.shapes.Shapeable
 require_relative './mixins/movable'
 class Pacman < BasicPacmanComponent
   include Movable
+  include Shapeable
+  attr_accessor :shape
 
   def update(delta_state)
     move(delta_state)
+    super
+  end
+
+  java_signature 'double getX()'
+  def getX
+    super
+  end
+
+  java_signature 'double getY()'
+  def getY
     super
   end
 end
--- pacman-reloaded/src/main/java/launcher/launcher.rb
+++ pacman-reloaded/src/main/java/launcher/launcher.rb
@@ -6,11 +6,14 @@ require '../components/components_builder'
 
 java_import com.uqbar.vainilla.DesktopGameLauncher
 java_import Java::pacman.PacmanGame
+java_import Java::extensions.shapes.Circle
 
 c = Config.new 'application.xml'
 image = Resource.get_image 'pacman-min.png'
 pacman = ComponentsBuilder.new(c).create_pacman(image, 0, 1)
 pacman.initialize_vector 1, 0, 200
+pacman.shape = Circle.new (c.get_int('blockSize') - 1) /2, 0, 0
+pacman.shape.shapeable = pacman
 scene = SceneBuilder.new(c).with_background('img/map.png').with_walkable_matrix('img/map.png')
 .with_component(pacman).build
 game = PacmanGameBuilder.new.with_config(c).with_current_scene(scene).build
