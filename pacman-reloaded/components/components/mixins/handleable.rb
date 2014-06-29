23d1ca190b9713358a73fd79ea532b0837a0797f
lalo <leandro.gz73@gmail.com>
2014-06-29 00:48:57 -0300

    don stop pacman if it cannot change direction

--- pacman-reloaded/components/components/mixins/handleable.rb
+++ pacman-reloaded/components/components/mixins/handleable.rb
@@ -4,6 +4,8 @@ java_import Java::com.uqbar.vainilla.events.constants.Key
 
 module Handleable
   def update_vector(delta_state, vector)
+    old_y = vector.y
+    old_x = vector.x
     if delta_state.key_pressed? Key::UP
       vector.y = -1
       vector.x = 0
@@ -19,6 +21,17 @@ module Handleable
     if delta_state.key_pressed? Key::LEFT
       vector.x = -1
       vector.y = 0
+    end
+    unless can_move? delta_state
+      if old_y == 0
+        vector.x = old_x
+        vector.y = old_y
+      else
+        vector.y = old_y
+        vector.x = old_x
+      end
+
+
     end
   end
 end
