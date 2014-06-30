7fb45760f6686927440f0bde9e9f4d5722fd49df
lgomez <lgomez@devartis.com>
2014-06-30 16:09:34 -0300

    take logic to simple movements class

--- pacman-reloaded/components/components/ghosts/ghost.rb
+++ pacman-reloaded/components/components/ghosts/ghost.rb
@@ -1,38 +1,22 @@
 require_relative '../basic_pacman_component'
+require_relative './simple_movement'
 
 class Ghost < BasicPacmanComponent
-  attr_accessor :count, :rotation
+  attr_accessor :mover
 
   def initialize(shape, appearance, x, y, x_vector, y_vector, speed)
     super
-    self.count = 0
-    self.rotation = -1
+    self.mover = SimpleMovement.new
   end
 
 
   def update(delta_state)
     fix_position
     if can_move? delta_state
-      move(delta_state)
+      mover.move(self, delta_state)
     else
-      rotate
-      self.count = count + 1
-      if count == 3
-        self.count = 0
-        self.rotation *= -1
-      end
+      mover.rotate self
     end
-  end
-
-  def rotate
-    #TODO: GOD! This scares
-      if uVector.x == 0
-        uVector.x = uVector.y * rotation
-        uVector.y = 0
-      else
-        uVector.y = uVector.x * rotation
-        uVector.x = 0
-      end
   end
 
 
--- pacman-reloaded/components/components/ghosts/simple_movement.rb
+++ pacman-reloaded/components/components/ghosts/simple_movement.rb
@@ -1,6 +1,30 @@
 class SimpleMovement
+  attr_accessor :count, :rotation
 
+  def initialize
+    self.count = 0
+    self.rotation = -1
+  end
 
+  def move(ghost, delta_state)
+    ghost.move delta_state
+  end
+
+  def rotate(ghost)
+    #TODO: GOD! This scares
+    if ghost.uVector.x == 0
+      ghost.uVector.x = ghost.uVector.y * rotation
+      ghost.uVector.y = 0
+    else
+      ghost.uVector.y = ghost.uVector.x * rotation
+      ghost.uVector.x = 0
+    end
+    self.count = count + 1
+    if count == 3
+      self.count = 0
+      self.rotation *= -1
+    end
+  end
 
 
 end
