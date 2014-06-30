8c75050ef73d23ae502acde0155dd47b5f4fb487
Leandro Gomez <leandro.gz73@gmail.com>
2014-06-30 00:52:45 -0300

    minor refactor

--- pacman-reloaded/components/components/ghosts/ghost.rb
+++ pacman-reloaded/components/components/ghosts/ghost.rb
@@ -3,15 +3,15 @@ require_relative '../basic_pacman_component'
 class Ghost < BasicPacmanComponent
   attr_accessor :count, :rotation
 
+  def initialize(shape, appearance, x, y, x_vector, y_vector, speed)
+    super
+    self.count = 0
+    self.rotation = -1
+  end
+
 
   def update(delta_state)
     fix_position
-    if count.nil?
-      self.count = 0
-    end
-    if rotation.nil?
-      self.rotation = -1
-    end
     if can_move? delta_state
       move(delta_state)
     else
@@ -25,6 +25,7 @@ class Ghost < BasicPacmanComponent
   end
 
   def rotate
+    #TODO: GOD! This scares
     if rotation < 0
       if uVector.x == 0
         if uVector.y > 0
